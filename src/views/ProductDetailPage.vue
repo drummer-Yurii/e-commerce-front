a661107bde373f10f8331b484e409f01ec7b2a7c
Yurii Rybachok <drummer2009@ukr.net>
2023-05-11 10:42:59 +0300

    improve the add to cart interface

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -9,9 +9,17 @@
             <p>Average rating: {{ product.averageRating }}</p>
             <button 
               id="add-to-cart"
+              v-if="!showSuccessMessage"
               @click="addToCart"
             >
               Add to Cart
+            </button>
+            <button 
+              id="add-to-cart"
+              class="green-button"
+              v-if="showSuccessMessage"
+            >
+              Successfully added item to cart!
             </button>
             <h4>Description</h4>
             <p>{{ product.description }}</p>
@@ -32,6 +40,7 @@ export default {
     data() {
         return {
             product: {},
+            showSuccessMessage: false,
         };
     },
 
@@ -40,6 +49,10 @@ export default {
         await axios.post('/api/users/12345/cart', {
           productId: this.$route.params.id,
         });
+        this.showSuccessMessage = true;
+        setTimeout(() => {
+          this.$router.push('/products');
+        }, 1500);
       },
     },
     async created() {
@@ -78,5 +91,9 @@ export default {
     position: absolute;
     top: 24px;
     right: 16px;
+  }
+
+  .green-button {
+    background-color: green;
   }
 </style>
